32c62f913f64468980de08c25900dbc6c5538000
chenzirui <928343544@qq.com>
2023-02-25 09:21:18 +0800

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+main() {
+  runApp(const Center(
+      child: Text(
+    "到此一游",
+    textDirection: TextDirection.ltr,
+        style: TextStyle(),
+  )));
+}
